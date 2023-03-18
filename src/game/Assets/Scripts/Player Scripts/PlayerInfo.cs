0a68c9532d87a9b8347a712c3c1e3f833c585305
Robbie Hammond <robbiehammond3@gmail.com>
2023-03-17 21:24:45 -0400

    added player animations
    a bit wonky, but the logic is all there

--- src/game/Assets/Scripts/Player Scripts/PlayerInfo.cs
+++ src/game/Assets/Scripts/Player Scripts/PlayerInfo.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
+    MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    IDLE_UP,
+    IDLE_DOWN,
+    IDLE_LEFT,
+    IDLE_RIGHT
 }
-
 
 //Singleton so the same information is seen everywhere
 public sealed class PlayerInfo {
@@ -18,7 +21,7 @@ public sealed class PlayerInfo {
 
 
     private PlayerInfo() {
-        mostRecentDirection = Direction.DOWN;
+        mostRecentDirection = Direction.IDLE_DOWN;
     }
     public static PlayerInfo Instance {
         get {
@@ -29,42 +32,64 @@ public sealed class PlayerInfo {
             }
         }
     }
+
+    //for the sake of making the animation look good. Could probably be simplified a bit.
     public void setLastDirection(float prevDX, float prevDY, float dX, float dY) {
+        //idling 
+        if (dX == 0 && dY == 0) {
+            Direction prevDir = mostRecentDirection;
+            switch (prevDir) {
+                case Direction.MOVE_LEFT:
+                    mostRecentDirection = Direction.IDLE_LEFT;
+                    break;
+                case Direction.MOVE_RIGHT:
+                    mostRecentDirection = Direction.IDLE_RIGHT;
+                    break;
+                case Direction.MOVE_UP:
+                    mostRecentDirection = Direction.IDLE_UP;
+                    break;
+                case Direction.MOVE_DOWN:
+                    mostRecentDirection = Direction.IDLE_DOWN;
+                    break;
+
+            }
+        }
+
+
         //if you start moving horizontally
         if (dX != 0 && dX != prevDX) {
             if (dX > 0)
-                mostRecentDirection = Direction.RIGHT;
+                mostRecentDirection = Direction.MOVE_RIGHT;
             else if (dX < 0)
-                mostRecentDirection = Direction.LEFT;
+                mostRecentDirection = Direction.MOVE_LEFT;
         }
 
         //if you start moving vertically
         if (dY != 0 && dY != prevDY) {
             if (dY > 0)
-                mostRecentDirection = Direction.UP;
+                mostRecentDirection = Direction.MOVE_UP;
             else if (dY < 0)
-                mostRecentDirection = Direction.DOWN;
+                mostRecentDirection = Direction.MOVE_DOWN;
         }
 
         //if you let go of a horizontal keypress
         if (dX == 0 && dY != 0) {
             if (dY > 0)
-                mostRecentDirection = Direction.UP;
+                mostRecentDirection = Direction.MOVE_UP;
             else if (dY < 0)
-                mostRecentDirection = Direction.DOWN;
+                mostRecentDirection = Direction.MOVE_DOWN;
         }
 
         //if you let go of a vertical keypress
         if (dY == 0 && dX != 0) {
             if (dX > 0)
-                mostRecentDirection = Direction.RIGHT;
+                mostRecentDirection = Direction.MOVE_RIGHT;
             else if (dX < 0)
-                mostRecentDirection = Direction.LEFT;
+                mostRecentDirection = Direction.MOVE_LEFT;
         }
     }
 
     public Direction getLastDirection() {
         return mostRecentDirection;
     }
-
 }
--- src/game/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ src/game/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d;
     public float speed = 100.0f;
     private PlayerInfo playerInformation = PlayerInfo.Instance;
+    public Animator animator;
     
     //to see which direction is the most "recent" press
     private float prevDeltaX = 0.0f;
@@ -23,12 +24,18 @@ public class PlayerMovement : MonoBehaviour
         //get change in X and Y
         float deltaX = Input.GetAxisRaw("Horizontal") * speed;
         float deltaY = Input.GetAxisRaw("Vertical") * speed;
+        Direction lastDir = playerInformation.getLastDirection();
         playerInformation.setLastDirection(prevDeltaX, prevDeltaY, deltaX, deltaY);
+
+        //only indicate to animator when the direction is actually changed.
+        //sending a message each time causes the animation to reset each frame, which is obviously bad.
+        if (lastDir != playerInformation.getLastDirection())
+            triggerAnimator(playerInformation.getLastDirection());
+
 
         Vector2 delta = new Vector2(deltaX, deltaY);
 
-        Debug.Log("dX: " + deltaX + " dY: " + deltaY);
-        Debug.Log(playerInformation.getLastDirection());
+        //Debug.Log(playerInformation.getLastDirection());
 
         //make it FPS invariant
         delta *= Time.deltaTime;
@@ -39,5 +46,10 @@ public class PlayerMovement : MonoBehaviour
 
         prevDeltaX = deltaX;
         prevDeltaY = deltaY;
+    }
+
+
+    private void triggerAnimator(Direction d) {
+        animator.SetTrigger(d.ToString());
     }
 }
